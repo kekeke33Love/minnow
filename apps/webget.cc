490d6235d28af5ebdaa5a1f52f7c68ed27540ace
ee <3039305687@qq.com>
2025-03-03 20:42:03 +0800

    backup

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,21 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket;
+  socket.connect(Address(host, "http"));
+  socket.write("GET " + path + " HTTP/1.1\r\n");
+  socket.write("Host: " + host + "\r\n");
+  socket.write("Connection: close\r\n\r\n");
+  string str;
+  while(!socket.eof())
+  {
+    socket.read(str);
+    cout << str;
+  }
+  socket.close();
+
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,55 +2,73 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), close_flag_( false ), bytes_pushed_( 0 ), bytes_popped_( 0 ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  if( has_error() || is_closed() )
+  {
+    return;
+  }
+
+  uint64_t len = min( data.length(), available_capacity() );
+  buf_.append( data, 0, len);
+  bytes_pushed_ += len;
+
 }
 
 void Writer::close()
 {
-  // Your code here.
+  close_flag_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return close_flag_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - ( bytes_pushed_ - bytes_popped_ );
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return this->buf_;
+  // return string_view( buf_ ).substr( 0, 1 );
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  if(bytes_buffered() == 0)
+  {
+    return;
+  }
+  len = min( len, bytes_buffered() );
+  buf_.erase( 0, len);
+  bytes_popped_ += len;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  if(close_flag_ && bytes_buffered() == 0 )
+  {
+    return true;
+  }
+  return false;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_ - bytes_popped_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
-
